7caa53d9b78d14357c2c5b2e10f6e8138bd27122
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2024-12-11 14:51:51 -0500

    sum forecast by product o url fetch with api

--- frontend/mps/src/components/PlannerTable.js
+++ frontend/mps/src/components/PlannerTable.js
@@ -1,161 +1,278 @@
-import React, { useState } from 'react';
-import '../styles/PlannerTable.css'; // Asegúrate de importar el archivo CSS
-import { formatNumber } from '../utils/helpers'; // Importa la función de formato
+// import React, { useState } from 'react';
+// import '../styles/PlannerTable.css'; // Asegúrate de importar el archivo CSS
+// import { formatNumber } from '../utils/helpers'; // Importa la función de formato
 
-const PlannerTable = ({ data, onCellEdit, sku, description }) => {
-  const [editingValue, setEditingValue] = useState({});
-  const [tableData, setTableData] = useState(data);
+// const PlannerTable = ({ data, onCellEdit, sku, description }) => {
+//   const [editingValue, setEditingValue] = useState({});
+//   const [tableData, setTableData] = useState(data);
 
-  const handleCellEdit = (rowIndex, field, value) => {
+//   const handleCellEdit = (rowIndex, field, value) => {
   
-    const numericValue =value;
-    const newData = [...tableData];
+//     const numericValue =value;
+//     const newData = [...tableData];
   
-    newData[rowIndex][field] = numericValue;
-    setTableData(newData);
-    onCellEdit(rowIndex, field, numericValue);
+//     newData[rowIndex][field] = numericValue;
+//     setTableData(newData);
+//     onCellEdit(rowIndex, field, numericValue);
     
-    calculateFinalInventory(newData); // Llama a calculateFinalInventory solo cuando se edita una celda
+//     calculateFinalInventory(newData); // Llama a calculateFinalInventory solo cuando se edita una celda
 
+//   };
+
+//   // const handleInputChange = (rowIndex, field, value) => {
+//   //   const numericValue = value.replace(/,/g, '');
+//   //   setEditingValue({ ...editingValue, [`${rowIndex}-${field}`]: numericValue });
+//   // };
+
+//   // const handleInputBlur = (rowIndex, field) => {
+//   //   const value = editingValue[`${rowIndex}-${field}`];
+//   //   // handleCellEdit(rowIndex, field, value);
+//   //   setEditingValue({ ...editingValue, [`${rowIndex}-${field}`]: formatNumber(value) });
+//   // };
+
+//   const calculateFinalInventory = (newData) => {
+//     for (let i = 0; i < newData.length; i++) {
+//       if (newData[i].name === "Inventario final"){
+//         newData[6].m1=newData[6].entradas+newData[5].m1+newData[4].m1-newData[0].m1; //inventario final m1
+//         newData[6].m2= newData[6].m1+newData[5].m2+newData[4].m2-newData[0].m2; //inventario final m2
+//         newData[6].m3= newData[6].m2+newData[5].m3+newData[4].m3-newData[0].m3;//inventario final m3
+//         newData[6].m4= newData[6].m3+newData[5].m4+newData[4].m4-newData[0].m4;//inventario final m4
+//         newData[6].m5= newData[6].m4+newData[5].m5+newData[4].m5-newData[0].m5;//inventario final m5
+//         newData[6].m6= newData[6].m5+newData[5].m6+newData[4].m6-newData[0].m6;//inventario final m6
+       
+//       }
+//     }
+//     setTableData(newData);
+//     //newData[5].m1= formatNumber(newData[5].m1);
+    
+//   };
+
+//   return (
+//     <table>
+//       <thead>
+//         <tr>
+//           <th colSpan="8" className="left-align">SKU: {sku}</th>
+//         </tr>
+//         <tr>
+//           <th colSpan="8" className="left-align">Descripción: {description} </th>
+//         </tr>
+//         <tr>
+//           <th className="left-align">Concepto</th>
+//           <th className="left-align">Datos Iniciales</th>
+//           <th className="center-align">Mes 1</th>
+//           <th className="center-align">Mes 2</th>
+//           <th className="center-align">Mes 3</th>
+//           <th className="center-align">Mes 4</th>
+//           <th className="center-align">Mes 5</th>
+//           <th className="center-align">Mes 6</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {tableData
+//         .filter(row => row.id == 3)
+//         .map((row, rowIndex) => (
+//           <tr key={row.id} className={row.name === "Cantidad a Producir" ? "cantidad-a-producir" : ""}>
+//             <td className="left-align">{row.name}</td>
+//             <td className="center-align">{formatNumber(row.entradas)}</td>
+//             <td className="center-align">
+//               {row.editable ? (
+//                 <input
+//                   type="text"
+//                   value={(row.m1)}
+//                   //onChange={(e) => handleInputChange(rowIndex, 'm1', e.target.value)}
+//                 //  onBlur={() => handleInputBlur(rowIndex, 'm1')}
+//                  onChange={(e) => handleCellEdit(rowIndex, 'm1', e.target.value)}
+//                   className="center-align"
+//                 />
+//               ) : (
+//                 console.log("No entra", row.m1)
+//               )}
+//             </td>
+//             <td className="center-align">
+//               {row.editable ? (
+//                 <input
+//                   type="text"
+//                   value={(row.m2)}
+//                   // onChange={(e) => handleInputChange(rowIndex, 'm2', e.target.value)}
+//                   // onBlur={() => handleInputBlur(rowIndex, 'm2')}
+//                   onChange={(e) => handleCellEdit(rowIndex, 'm2', e.target.value)}
+//                   className="center-align"
+//                 />
+//               ) : (
+//                 formatNumber(row.m2)
+//               )}
+//             </td>
+//             <td className="center-align">
+//               {row.editable ? (
+//                 <input
+//                   type="text"
+//                    value={ (row.m3)}
+//                   // onChange={(e) => handleInputChange(rowIndex, 'm3', e.target.value)}
+//                   // onBlur={() => handleInputBlur(rowIndex, 'm3')}
+//                   onChange={(e) => handleCellEdit(rowIndex, 'm3', e.target.value)}
+//                   className="center-align"
+//                 />
+//               ) : (
+//                 formatNumber(row.m3)
+//               )}
+//             </td>
+//             <td className="center-align">
+//               {row.editable ? (
+//                 <input
+//                   type="text"
+//                    value={ (row.m4)}
+//                   // onChange={(e) => handleInputChange(rowIndex, 'm4', e.target.value)}
+//                   // onBlur={() => handleInputBlur(rowIndex, 'm4')}
+//                   onChange={(e) => handleCellEdit(rowIndex, 'm4', e.target.value)}
+//                   className="center-align"
+//                 />
+//               ) : (
+//                 formatNumber(row.m4)
+//               )}
+//             </td>
+//             <td className="center-align">
+//               {row.editable ? (
+//                 <input
+//                   type="text"
+//                    value={ (row.m5)}
+//                   // onChange={(e) => handleInputChange(rowIndex, 'm5', e.target.value)}
+//                   // onBlur={() => handleInputBlur(rowIndex, 'm5')}
+//                   onChange={(e) => handleCellEdit(rowIndex, 'm5', e.target.value)}
+//                   className="center-align"
+//                 />
+//               ) : (
+//                 formatNumber(row.m5)
+//               )}
+//             </td>
+//             <td className="center-align">
+//               {row.editable ? (
+//                 <input
+//                   type="text"
+//                   value={(row.m6)}
+//                   // onChange={(e) => handleInputChange(rowIndex, 'm6', e.target.value)}
+//                   // onBlur={() => handleInputBlur(rowIndex, 'm6')}
+//                   onChange={(e) => handleCellEdit(rowIndex, 'm6', e.target.value)}
+//                   className="center-align"
+//                 />
+//               ) : (
+//                 formatNumber(row.m6)
+//               )}
+//             </td>
+//           </tr>
+//         ))}
+//       </tbody>
+//     </table>
+//   );
+// };
+
+// export default PlannerTable;
+
+//AQUI
+import React, { useState } from 'react';
+
+const PlannerTable = ({ data, onCellEdit }) => {
+  const [editingValue, setEditingValue] = useState({});
+
+  const handleInputChange = (rowIndex, field, value) => {
+    const numericValue = value.replace(/,/g, '');
+    setEditingValue({ ...editingValue, [`${rowIndex}-${field}`]: numericValue });
   };
 
-  // const handleInputChange = (rowIndex, field, value) => {
-  //   const numericValue = value.replace(/,/g, '');
-  //   setEditingValue({ ...editingValue, [`${rowIndex}-${field}`]: numericValue });
-  // };
-
-  // const handleInputBlur = (rowIndex, field) => {
-  //   const value = editingValue[`${rowIndex}-${field}`];
-  //   // handleCellEdit(rowIndex, field, value);
-  //   setEditingValue({ ...editingValue, [`${rowIndex}-${field}`]: formatNumber(value) });
-  // };
-
-  const calculateFinalInventory = (newData) => {
-    for (let i = 0; i < newData.length; i++) {
-      if (newData[i].name === "Inventario final"){
-        newData[6].m1=newData[6].entradas+newData[5].m1+newData[4].m1-newData[0].m1; //inventario final m1
-        newData[6].m2= newData[6].m1+newData[5].m2+newData[4].m2-newData[0].m2; //inventario final m2
-        newData[6].m3= newData[6].m2+newData[5].m3+newData[4].m3-newData[0].m3;//inventario final m3
-        newData[6].m4= newData[6].m3+newData[5].m4+newData[4].m4-newData[0].m4;//inventario final m4
-        newData[6].m5= newData[6].m4+newData[5].m5+newData[4].m5-newData[0].m5;//inventario final m5
-        newData[6].m6= newData[6].m5+newData[5].m6+newData[4].m6-newData[0].m6;//inventario final m6
-       
-      }
-    }
-    setTableData(newData);
-    //newData[5].m1= formatNumber(newData[5].m1);
-    
+  const handleInputBlur = (rowIndex, field) => {
+    const value = editingValue[`${rowIndex}-${field}`];
+    onCellEdit(rowIndex, field, value);
+    setEditingValue({ ...editingValue, [`${rowIndex}-${field}`]: formatNumber(value) });
   };
 
   return (
     <table>
       <thead>
         <tr>
-          <th colSpan="8" className="left-align">SKU: {sku}</th>
-        </tr>
-        <tr>
-          <th colSpan="8" className="left-align">Descripción: {description} </th>
-        </tr>
-        <tr>
-          <th className="left-align">Concepto</th>
-          <th className="left-align">Datos Iniciales</th>
-          <th className="center-align">Mes 1</th>
-          <th className="center-align">Mes 2</th>
-          <th className="center-align">Mes 3</th>
-          <th className="center-align">Mes 4</th>
-          <th className="center-align">Mes 5</th>
-          <th className="center-align">Mes 6</th>
+          <th>Concepto</th>
+          <th>Datos Iniciales</th>
+          <th>Mes 1</th>
+          <th>Mes 2</th>
+          <th>Mes 3</th>
+          <th>Mes 4</th>
+          <th>Mes 5</th>
+          <th>Mes 6</th>
         </tr>
       </thead>
       <tbody>
-        {tableData
-        .filter(row => row.id >= 3)
-        .map((row, rowIndex) => (
-          <tr key={row.id} className={row.name === "Cantidad a Producir" ? "cantidad-a-producir" : ""}>
-            <td className="left-align">{row.name}</td>
-            <td className="center-align">{formatNumber(row.entradas)}</td>
-            <td className="center-align">
+        {data.map((row, rowIndex) => (
+          <tr key={row.id}>
+            <td>{row.name}</td>
+            <td>{row.entradas}</td>
+            <td>
               {row.editable ? (
                 <input
                   type="text"
-                  value={(row.m1)}
-                  //onChange={(e) => handleInputChange(rowIndex, 'm1', e.target.value)}
-                //  onBlur={() => handleInputBlur(rowIndex, 'm1')}
-                 onChange={(e) => handleCellEdit(rowIndex, 'm1', e.target.value)}
-                  className="center-align"
+                  value={editingValue[`${rowIndex}-m1`] || row.m1}
+                  onChange={(e) => handleInputChange(rowIndex, 'm1', e.target.value)}
+                  onBlur={() => handleInputBlur(rowIndex, 'm1')}
                 />
               ) : (
-                formatNumber(row.m1)
+                row.m1
               )}
             </td>
-            <td className="center-align">
+            <td>
               {row.editable ? (
                 <input
                   type="text"
-                  value={(row.m2)}
-                  // onChange={(e) => handleInputChange(rowIndex, 'm2', e.target.value)}
-                  // onBlur={() => handleInputBlur(rowIndex, 'm2')}
-                  onChange={(e) => handleCellEdit(rowIndex, 'm2', e.target.value)}
-                  className="center-align"
+                  value={editingValue[`${rowIndex}-m2`] || row.m2}
+                  onChange={(e) => handleInputChange(rowIndex, 'm2', e.target.value)}
+                  onBlur={() => handleInputBlur(rowIndex, 'm2')}
                 />
               ) : (
-                formatNumber(row.m2)
+                row.m2
               )}
             </td>
-            <td className="center-align">
+            <td>
               {row.editable ? (
                 <input
                   type="text"
-                   value={ (row.m3)}
-                  // onChange={(e) => handleInputChange(rowIndex, 'm3', e.target.value)}
-                  // onBlur={() => handleInputBlur(rowIndex, 'm3')}
-                  onChange={(e) => handleCellEdit(rowIndex, 'm3', e.target.value)}
-                  className="center-align"
+                  value={editingValue[`${rowIndex}-m3`] || row.m3}
+                  onChange={(e) => handleInputChange(rowIndex, 'm3', e.target.value)}
+                  onBlur={() => handleInputBlur(rowIndex, 'm3')}
                 />
               ) : (
-                formatNumber(row.m3)
+                row.m3
               )}
             </td>
-            <td className="center-align">
+            <td>
               {row.editable ? (
                 <input
                   type="text"
-                   value={ (row.m4)}
-                  // onChange={(e) => handleInputChange(rowIndex, 'm4', e.target.value)}
-                  // onBlur={() => handleInputBlur(rowIndex, 'm4')}
-                  onChange={(e) => handleCellEdit(rowIndex, 'm4', e.target.value)}
-                  className="center-align"
+                  value={editingValue[`${rowIndex}-m4`] || row.m4}
+                  onChange={(e) => handleInputChange(rowIndex, 'm4', e.target.value)}
+                  onBlur={() => handleInputBlur(rowIndex, 'm4')}
                 />
               ) : (
-                formatNumber(row.m4)
+                row.m4
               )}
             </td>
-            <td className="center-align">
+            <td>
               {row.editable ? (
                 <input
                   type="text"
-                   value={ (row.m5)}
-                  // onChange={(e) => handleInputChange(rowIndex, 'm5', e.target.value)}
-                  // onBlur={() => handleInputBlur(rowIndex, 'm5')}
-                  onChange={(e) => handleCellEdit(rowIndex, 'm5', e.target.value)}
-                  className="center-align"
+                  value={editingValue[`${rowIndex}-m5`] || row.m5}
+                  onChange={(e) => handleInputChange(rowIndex, 'm5', e.target.value)}
+                  onBlur={() => handleInputBlur(rowIndex, 'm5')}
                 />
               ) : (
-                formatNumber(row.m5)
+                row.m5
               )}
             </td>
-            <td className="center-align">
+            <td>
               {row.editable ? (
                 <input
                   type="text"
-                  value={(row.m6)}
-                  // onChange={(e) => handleInputChange(rowIndex, 'm6', e.target.value)}
-                  // onBlur={() => handleInputBlur(rowIndex, 'm6')}
-                  onChange={(e) => handleCellEdit(rowIndex, 'm6', e.target.value)}
-                  className="center-align"
+                  value={editingValue[`${rowIndex}-m6`] || row.m6}
+                  onChange={(e) => handleInputChange(rowIndex, 'm6', e.target.value)}
+                  onBlur={() => handleInputBlur(rowIndex, 'm6')}
                 />
               ) : (
-                formatNumber(row.m6)
+                row.m6
               )}
             </td>
           </tr>
--- frontend/mps/src/pages/plannertable.js
+++ frontend/mps/src/pages/plannertable.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import PlannerTable from '../components/PlannerTable';
 import Header from '../components/Header';
 import Chart from '../components/Chart';
 
 const HomePage = () => {
   const [tableData, setTableData] = useState([
-    { id: 1, name: 2400026101},
+    { id: 1, name: 123456789},
     { id: 2, name: "PLATO PANDO ESPUMADO 26 WAU! BLANCO PSE 480  20"},
-    { id: 3, name: "Pronóstico", entradas: "", m1: 80000, m2: 70000 ,m3:70000,m4:70000,m5:0,m6:0,editable:false },
+    { id: 3, name: "Pronóstico", entradas: "", m1: "", m2: 70000 ,m3:70000,m4:70000,m5:0,m6:0,editable:false },
     { id: 4, name: "Forecast Adicional", entradas: "", m1: "", m2: "" ,m3:"",m4:"",m5:"",m6:"",editable:false },
     { id: 5, name: "Pedidos de venta", entradas: "", m1: "", m2: "" ,m3:"",m4:"",m5:"",m6:"",editable:false},
     { id: 6, name: "Despachos", entradas: "", m1: "", m2: "" ,m3:"",m4:"",m5:"",m6:"",editable:false },
@@ -17,6 +18,64 @@ const HomePage = () => {
     { id: 10, name: "Días de inventario", entradas: "", m1: 0, m2: 0 ,m3:0,m4:0,m5:0,m6:0,editable:false },
   ]);
 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/products/forecastplanning/2400026101/forecasts/');
+        const forecasts = response.data;
+        console.log("forecasts a entrar",forecasts.find(f => f.period === '2025-01-01').forecast_value);
+
+       // Actualizar tableData con los datos de forecasts
+       const updatedTableData = tableData.map(row => {
+        if (row.name === "Pronóstico") {
+          const m1 = forecasts
+            .filter(f => f.period === '2025-01-01')
+            .reduce((acc, curr) => acc + parseFloat(curr.forecast_value), 0) || row.m1;
+          const m2 = forecasts
+            .filter(f => f.period === '2025-02-01')
+            .reduce((acc, curr) => acc + parseFloat(curr.forecast_value), 0) || row.m2;
+          const m3 = forecasts
+            .filter(f => f.period === '2025-03-01')
+            .reduce((acc, curr) => acc + parseFloat(curr.forecast_value), 0) || row.m3;
+          const m4 = forecasts
+            .filter(f => f.period === '2025-04-01')
+            .reduce((acc, curr) => acc + parseFloat(curr.forecast_value), 0) || row.m4;
+          const m5 = forecasts
+            .filter(f => f.period === '2025-05-01')
+            .reduce((acc, curr) => acc + parseFloat(curr.forecast_value), 0) || row.m5;
+          const m6 = forecasts
+            .filter(f => f.period === '2025-06-01')
+            .reduce((acc, curr) => acc + parseFloat(curr.forecast_value), 0) || row.m6;
+
+          // console.log("m1:", m1, "m2:", m2, "m3:", m3, "m4:", m4, "m5:", m5, "m6:", m6);
+
+          return {
+            ...row,
+            m1,
+            m2,
+            m3,
+            m4,
+            m5,
+            m6,
+          };
+        }
+        return row;
+      });
+
+        setTableData(updatedTableData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+
+
+
   const handleCellEdit = (rowIndex, field, value) => {
     const newData = [...tableData];
     // console.log("handle",value);
@@ -25,9 +84,9 @@ const HomePage = () => {
     setTableData(newData);
   };
 
-  useEffect(() => {
-    // Aquí puedes agregar lógica para persistir los datos en una base de datos o API
-  }, [tableData]);
+  // useEffect(() => {
+  //   // Aquí puedes agregar lógica para persistir los datos en una base de datos o API
+  // }, [tableData]);
 
   const sku = tableData.find(row => row.id === 1)?.name || "";
   const description = tableData.find(row => row.id === 2)?.name || "";
